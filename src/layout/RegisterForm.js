58c8b4ae28e21a8c694acff7c80824ae32bb3fd4
Yan Jiang <jarodzz@gmail.com>
2021-12-20 23:31:54 +0800

    fix bugs - first successfully encrypted at sgx

--- src/layout/RegisterForm.js
+++ src/layout/RegisterForm.js
@@ -18,22 +18,23 @@ export default function RegisterForm(props) {
   const [sealStep2, setSealStep2] = useState(0);
   const [sealStep3, setSealStep3] = useState(0);
 
+
   function generateKey() {
-    let keyPair = window.crypto.subtle.generateKey(
+    window.crypto.subtle.generateKey(
       {
         name: "RSA-OAEP",
-        modulusLength: 4096,
-        publicExponent: new Uint8Array([1, 0, 1]),
+        modulusLength: 2048,
+        publicExponent: new Uint8Array([0x01, 0x00, 0x01]),
         hash: "SHA-256"
       },
       true,
       ["encrypt", "decrypt"]
-    );
-    console.log(keyPair);
-    keyPair.then((keyPair) => {
-      const pubk = window.crypto.subtle.exportKey("jwk", keyPair.publicKey);
-      pubk.then(a => setLocalPubK(a));
+    ).then((keyPair) => {
       setLocalPriK(keyPair.privateKey);
+      return exportCryptoKey(keyPair.publicKey);
+    }).then((pubKey) => {
+      setLocalPubK(pubKey);
+      console.log(pubKey);
     })
   }
 
@@ -43,8 +44,10 @@ export default function RegisterForm(props) {
       return;
     }
     console.log(email, mobile, password, privateKey);
-    const socket = new WebSocket('ws://127.0.0.1:12345');
+    let socket = new WebSocket('wss://10.244.1.18:12346/register');
     socket.onopen = function (event) {
+      console.log("connected");
+      console.log(localPubK);
       socket.send(localPubK);
     };
     socket.onmessage = function (event) {
@@ -53,6 +56,7 @@ export default function RegisterForm(props) {
       console.log(event.data);
     }
     socket.onclose = function (event) {
+      console.log("disconnected.")
       alert('Submit Completed!');
     }
   }
@@ -126,4 +130,27 @@ export default function RegisterForm(props) {
       </Grid>
     </React.Fragment>
   );
+}
+
+
+/*
+Convert  an ArrayBuffer into a string
+from https://developers.google.com/web/updates/2012/06/How-to-convert-ArrayBuffer-to-and-from-String
+*/
+function ab2str(buf) {
+  return String.fromCharCode.apply(null, new Uint8Array(buf));
+}
+
+/*
+Export the given key and write it into the "exported-key" space.
+*/
+async function exportCryptoKey(key) {
+  const exported = await window.crypto.subtle.exportKey(
+    "spki",
+    key
+  );
+  const exportedAsString = ab2str(exported);
+  const exportedAsBase64 = window.btoa(exportedAsString);
+  const pemExported = `-----BEGIN PUBLIC KEY-----\n${exportedAsBase64}\n-----END PUBLIC KEY-----`;
+  return pemExported;
 }
