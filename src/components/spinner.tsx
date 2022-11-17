912aaa89388b817f64df9bc05c7eb5833ad2832a
zhangxu1161 <zhangxu1161@gmail.com>
2022-11-18 00:03:11 +0800

    change spinner size

--- src/components/spinner.tsx
+++ src/components/spinner.tsx
@@ -3,6 +3,6 @@ import BounceLoader from "react-spinners/BounceLoader";
 
 export default function Spinner() {
   return (
-    <BounceLoader color="#36d7b7" size={'20'} className={'inlie-block'} />
+    <BounceLoader color="#36d7b7" size={20} className={'inlie-block'} />
   )
 }
